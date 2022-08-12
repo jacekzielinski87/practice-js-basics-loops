9e416064add99a848a3565536774c814563e60d8
Jacek Zielinski <gandi34@wp.pl>
2022-08-12 06:48:38 +0200

    Js loops

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,14 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+if (randomNumber > 5){
+    for(let i=5;i <= randomNumber;i++){
+        console.log (i);
+    }
+    console.log ('wieksze');
+}
+else {
+    console.log ('wylosowana liczba jest zbyt mała,aby użyć pętli')
+};
+
+
--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,28 @@
 const x = 5;
 let result = 0;
+
+for (let i = 1; i<=x;i++){
+    console.log (result,i);
+    result = result + i;
+    console.log (result,i);
+}
+console.log (result);
+
+//while
+
+
+
+let j =0;
+while (j<=5) {
+    console.log(j);
+    j+=1;
+}
+
+
+
+
+
+
+
+
+
