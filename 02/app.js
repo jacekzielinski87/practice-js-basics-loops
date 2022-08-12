b9a87af43e43f2a31e426f48211147365805914c
Jacek Zielinski <gandi34@wp.pl>
2022-08-12 10:39:15 +0200

    02 done

--- 02/app.js
+++ 02/app.js
@@ -4,19 +4,23 @@ let result = 0;
 for (let i = 1; i<=x;i++){
     console.log (result,i);
     result = result + i;
-    console.log (result,i);
+    //console.log (result,i);
 }
 console.log (result);
 
 //while
 
-
-
-let j =0;
-while (j<=5) {
-    console.log(j);
-    j+=1;
+let sum=0;
+let k=1;
+while (k<=5) {
+    console.log (sum,k);
+    sum=sum+k;
+    k++;
 }
+
+ 
+
+
 
 
 
--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,10 @@
-const x = 10;
+const x = 5;
 let iteration = 0;
 let randomNumber = -1;
+
+let newNumber = Math.round(Math.random() * 10);
+console.log(newNumber);
+
+
+
+
