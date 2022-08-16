156789f559a7f9592b3179bfd987dea8f9f773fa
Jacek Zielinski <gandi34@wp.pl>
2022-08-16 16:55:20 +0200

    Number basic game while loop

--- 03/app.js
+++ 03/app.js
@@ -2,13 +2,12 @@ const x = 5;
 let iteration = 0;
 let randomNumber = -1;
 
-let newType = Math.round(Math.random() * 20);
-console.log(newType);
-
-
-if (newType===x) {
-    alert('This is our number!');
+while (randomNumber !== x) {
+    randomNumber=Math.round(Math.random() * 20);
+    iteration = iteration +1;
+    console.log (randomNumber);
 }
+console.log (iteration,'Próba');
 
 
 
